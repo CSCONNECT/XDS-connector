c9bd5b36718384a9b88fb58c517f67ac8737d9ba
Brian Storm Graversen <brian.s.graversen@alexandra.dk>
2014-08-13 09:45:35 +0200

    added a "toString" thingy

--- src/main/java/org/net4care/xdsconnector/Application.java
+++ src/main/java/org/net4care/xdsconnector/Application.java
@@ -1,7 +1,7 @@
 package org.net4care.xdsconnector;
 
-
 import org.net4care.xdsconnector.service.RetrieveDocumentSetResponseType;
+import org.net4care.xdsconnector.service.RetrieveDocumentSetResponseType.DocumentResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.ApplicationContext;
@@ -16,6 +16,16 @@ public class Application {
 		XDSConnector client = ctx.getBean(XDSConnector.class);
 
 		RetrieveDocumentSetResponseType response = client.get();
-		System.out.println(response);
+		System.out.println("docsize=" + response.getDocumentResponse().size());
+		if (response.getDocumentResponse().size() > 0) {
+			DocumentResponse documentResponse = response.getDocumentResponse().get(0);
+			System.out.println("documentId=" + documentResponse.getDocumentUniqueId());
+			System.out.println("communityId=" + documentResponse.getHomeCommunityId());
+			System.out.println("******");
+			System.out.println("document=" + new String(documentResponse.getDocument()));
+			System.out.println("******");
+		}
+		System.out.println("requestid=" + response.getRegistryResponse().getRequestId());
+		System.out.println("status=" + response.getRegistryResponse().getStatus());
     }
 }
