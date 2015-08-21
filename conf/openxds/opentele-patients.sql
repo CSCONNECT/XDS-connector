30d991c9c4ad58543da1309c49fe2053c81f6000
Torben Haagh <torben.haagh@alexandra.dk>
2015-08-21 11:39:44 +0200

    Updated patient SQL.

--- conf/openxds/opentele-patients.sql
+++ conf/openxds/opentele-patients.sql
@@ -7,6 +7,14 @@ INSERT INTO PersonIdentifier (registrypatientid, assigningauthority, patientid, 
 ('3e759882-3c40-11e5-a151-feff819cdc9f', '&1.2.208.176.1.2&ISO', '1206322639', 'N', 'N', null);
 
 INSERT INTO Person (id, home, lid, objecttype, status, versionname, comment_, personname_firstname, personname_middlename, personname_lastname) VALUES
+('2512484916^^^&1.2.208.176.1.2&ISO', '', '2512484916^^^&1.2.208.176.1.2&ISO', 'urn:oasis:names:tc:ebxml-regrep:ObjectType:RegistryObject:Person', 'urn:oasis:names:tc:ebxml-regrep:StatusType:Submitted', '1.0', 'Danish Test', 'Nancy Ann', '', 'Berggren'),
+('2512688916^^^&1.2.208.176.1.2&ISO', '', '2512688916^^^&1.2.208.176.1.2&ISO', 'urn:oasis:names:tc:ebxml-regrep:ObjectType:RegistryObject:Person', 'urn:oasis:names:tc:ebxml-regrep:StatusType:Submitted', '1.0', 'OpenTele Test', 'Lene', '', 'Jensen'),
+('1103811376^^^&1.2.208.176.1.2&ISO', '', '1103811376^^^&1.2.208.176.1.2&ISO', 'urn:oasis:names:tc:ebxml-regrep:ObjectType:RegistryObject:Person', 'urn:oasis:names:tc:ebxml-regrep:StatusType:Submitted', '1.0', 'OpenTele Test', 'Kiran', '', 'Liaqat'),
+('1212852635^^^&1.2.208.176.1.2&ISO', '', '1212852635^^^&1.2.208.176.1.2&ISO', 'urn:oasis:names:tc:ebxml-regrep:ObjectType:RegistryObject:Person', 'urn:oasis:names:tc:ebxml-regrep:StatusType:Submitted', '1.0', 'OpenTele Test', 'Svend', '', 'Andersson'),
+('1105491135^^^&1.2.208.176.1.2&ISO', '', '1105491135^^^&1.2.208.176.1.2&ISO', 'urn:oasis:names:tc:ebxml-regrep:ObjectType:RegistryObject:Person', 'urn:oasis:names:tc:ebxml-regrep:StatusType:Submitted', '1.0', 'OpenTele Test', 'Carl', '', 'Petersen'),
+('1206322639^^^&1.2.208.176.1.2&ISO', '', '1206322639^^^&1.2.208.176.1.2&ISO', 'urn:oasis:names:tc:ebxml-regrep:ObjectType:RegistryObject:Person', 'urn:oasis:names:tc:ebxml-regrep:StatusType:Submitted', '1.0', 'OpenTele Test', 'John', '', 'Hansen');
+
+INSERT INTO user_ (id, home, lid, objecttype, status, versionname, comment_, personname_firstname, personname_middlename, personname_lastname) VALUES
 ('2512484916^^^&1.2.208.176.1.2&ISO', '', '2512484916^^^&1.2.208.176.1.2&ISO', 'urn:oasis:names:tc:ebxml-regrep:ObjectType:RegistryObject:Person:User', 'urn:oasis:names:tc:ebxml-regrep:StatusType:Submitted', '1.0', 'Danish Test', 'Nancy Ann', '', 'Berggren'),
 ('2512688916^^^&1.2.208.176.1.2&ISO', '', '2512688916^^^&1.2.208.176.1.2&ISO', 'urn:oasis:names:tc:ebxml-regrep:ObjectType:RegistryObject:Person:User', 'urn:oasis:names:tc:ebxml-regrep:StatusType:Submitted', '1.0', 'OpenTele Test', 'Lene', '', 'Jensen'),
 ('1103811376^^^&1.2.208.176.1.2&ISO', '', '1103811376^^^&1.2.208.176.1.2&ISO', 'urn:oasis:names:tc:ebxml-regrep:ObjectType:RegistryObject:Person:User', 'urn:oasis:names:tc:ebxml-regrep:StatusType:Submitted', '1.0', 'OpenTele Test', 'Kiran', '', 'Liaqat'),
